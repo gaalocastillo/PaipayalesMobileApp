864732739fb5f04f628ceb3010f465477cb44025
adelovelace <andy_mplz@hotmail.com>
2019-02-25 12:19:50 -0500

    home and registration

---
+++ Paipay/myApp/src/app/registration/registration.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: 'registration.page.html',
+  styleUrls: ['registration.page.scss'],
+})
+export class RegistrationPage {
+
+}
+
