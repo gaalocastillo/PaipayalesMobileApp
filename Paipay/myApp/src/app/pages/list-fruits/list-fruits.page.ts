55bd06a67804a0d3aa87c624070e199ca800d37f
adelovelace <andy_mplz@hotmail.com>
2019-04-24 16:08:07 -0500

    add footer of the products: fruits, organics, rices and vegetables

--- Paipay/myApp/src/app/pages/list-fruits/list-fruits.page.ts
+++ Paipay/myApp/src/app/pages/list-fruits/list-fruits.page.ts
@@ -12,20 +12,22 @@ export class ListFruitsPage implements OnInit {
   public items  = []; 
   qty:any;
 
-  constructor( private http: HttpClient) { 
+  constructor( ) { 
     
-    this.qty = 1;
-
-    this.http.get('http://127.0.0.1:9000/api/v1/products/fruits').subscribe((response : any[]) => {
-      console.log(response);
-      this.items = response;
-      });
-  
-  
-      
+    this.items =[
+      {name:"Ciruela", img:"assets/fruits/58-02.png" ,price:1.0, qty:1},
+      {name:"Guaba", img:"assets/fruits/59-02.png" ,price:1.0, qty:1},
+      {name:"Guayaba", img:"assets/fruits/60-02.png" ,price:1.0, qty:1},
+      {name:"Limón", img:"assets/fruits/61-02.png" ,price:1.0, qty:1},
+      {name:"Mango paipay", img:"assets/fruits/62-02.png" ,price:1.0, qty:1},
+      {name:"Mango de chupar", img:"assets/fruits/67-02.png" ,price:1.0, qty:1},
+      {name:"Naranja", img:"assets/fruits/63-02.png" ,price:1.0, qty:1},
+      {name:"Platano verde", img:"assets/fruits/65-02.png" ,price:1.0, qty:1},
+      {name:"Platano", img:"assets/fruits/66-02.png" ,price:1.0, qty:1},
+      {name:"Tamarindo", img:"assets/fruits/64-02.png" ,price:1.0, qty:1}
+    ];
     
-      
-
+  
   }
 
 
